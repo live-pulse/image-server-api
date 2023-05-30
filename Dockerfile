98e64c89b7ea691f51a4f37698ea9ac40cb863af
Hannah <ghdcoalss33@gmail.com>
2023-05-30 14:47:41 +0900

    fix(cdn): add dockerfile and fix date format

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM openjdk:17-ea-33-jdk-buster
+WORKDIR /build
+
+# 그래들 파일이 변경되었을 때만 새롭게 의존패키지 다운로드 받게함 (파일 올릴 때 마다 gradle 다운로드 안하게 캐시 작업)
+COPY build.gradle.kts settings.gradle.kts /build/
+RUN gradle build -x test --parallel --continue > /dev/null 2>&1 || true
+
+# 빌더 이미지에서 애플리케이션 빌드
+COPY . /build/
+CMD ["gradle", "clean", "bootJar"]
+
+# APP
+FROM openjdk:17-ea-33-jdk-buster
+WORKDIR /target
+
+COPY /build/libs/image-server-api-0.0.1-SNAPSHOT.jar image-server-api.jar
+
+EXPOSE 8080
+#ENV	USE_PROFILE prod
+
+#ENTRYPOINT ["java", "-Dspring.profiles.active=${USE_PROFILE}", "-jar", "/target/backend-api.jar"]
+ENTRYPOINT ["java", "-jar", "/target/image-server-api.jar"]
--- src/main/kotlin/site/hannahlog/cdn/image/cdn/service/CdnService.kt
+++ src/main/kotlin/site/hannahlog/cdn/image/cdn/service/CdnService.kt
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service
 import site.hannahlog.cdn.image.cdn.dto.request.UploadPath
 import site.hannahlog.cdn.image.cdn.dto.request.UploadRequest
 import site.hannahlog.cdn.image.cdn.dto.response.UploadResponse
-import java.io.FileOutputStream
-import java.io.OutputStream
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -25,7 +23,7 @@ class CdnService {
     fun upload(request: UploadRequest): UploadResponse {
         val data: String? = try {
             val requestPath: String = UploadPath.of(request.path)
-            val fileName = "${Date()}_${request.file.originalFilename}"
+            val fileName = "${Date().time}_${request.file.originalFilename}"
             val path: Path = Paths.get(defaultPath.plus(requestPath), fileName)
             Files.write(path, request.file.bytes, StandardOpenOption.CREATE)
             "$serverDomain/${request.path}/$fileName"
