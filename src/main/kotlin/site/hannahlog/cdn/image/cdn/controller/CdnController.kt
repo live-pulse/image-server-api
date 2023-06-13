0c6255c452a98eb382941413fe89796dcdda815a
Hannah <ghdcoalss33@gmail.com>
2023-06-13 11:11:31 +0900

    feat(cors): end point 에도 설정 추가

--- src/main/kotlin/site/hannahlog/cdn/image/cdn/controller/CdnController.kt
+++ src/main/kotlin/site/hannahlog/cdn/image/cdn/controller/CdnController.kt
@@ -2,6 +2,7 @@ package site.hannahlog.cdn.image.cdn.controller
 
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -15,6 +16,7 @@ class CdnController(
     private val cdnService: CdnService
 ) {
 
+    @CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
     @PostMapping(value = ["/upload"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun upload(request: UploadRequest): ResponseEntity<UploadResponse> {
         val result = cdnService.upload(request)
