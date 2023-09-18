5753f3bed8dca4ce0660054912d10a7e8e7ba32f
Hannah <ghdcoalss33@gmail.com>
2023-09-18 18:05:19 +0900

    feat(blog): add blog path

--- src/main/kotlin/site/hannahlog/cdn/image/cdn/dto/request/UploadPath.kt
+++ src/main/kotlin/site/hannahlog/cdn/image/cdn/dto/request/UploadPath.kt
@@ -1,12 +1,13 @@
 package site.hannahlog.cdn.image.cdn.dto.request
 
 enum class UploadPath {
-    LIVE_PULSE, TEST;
+    LIVE_PULSE, BLOG, TEST;
 
     companion object {
         fun of(path: UploadPath): String {
             return when (path) {
                 LIVE_PULSE -> "live-pulse"
+                BLOG -> "blog"
                 TEST -> "test"
             }
         }
